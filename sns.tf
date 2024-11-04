a17fee3e40e665009636c233a4a44323d8e4797c
Augusto Valdivia <augustovaldivia@f2:d7:e3:54:cd:71.home>
2024-11-04 11:50:09 -0500

    added text message option

--- sns.tf
+++ sns.tf
@@ -5,9 +5,16 @@ resource "aws_sns_topic" "service_quota_alerts" {
   name = "service-quota-alerts"
 }
 
-# Create an SNS Subscription to send notifications to your email
+#Create an SNS Subscription to send notifications to your email
 resource "aws_sns_topic_subscription" "email_subscription" {
   topic_arn = aws_sns_topic.service_quota_alerts.arn
   protocol  = "email"
   endpoint  = "your_email@example.com"  # Replace with your email
+}
+
+# Create an SNS Subscription for SMS
+resource "aws_sns_topic_subscription" "sms_subscription" {
+  topic_arn = aws_sns_topic.service_quota_alerts.arn
+  protocol  = "sms"
+  endpoint  = "+16472224567"  # Replace with the phone number you want to subscribe
 }
